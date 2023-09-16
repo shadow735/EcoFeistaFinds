a00ead964b7774fb4459a0279aba64d6065cbcb7
roshanidubey <123582053+roshanidubey@users.noreply.github.com>
2023-09-17 00:26:32 +0530

    added summary file and one summary folder in admin and added file widget
    code in summary file and widget file

---
+++ src/admin/summary.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import {FaUsers,FaChartBar,FaClipboard} from "react-icons";
+import { Summary } from "./Summary.1";
+
+export default Summary;
+
+const StyledSummary = styled.div`
+ width: 100% ;
+ display: flex;
+ `;
+ 
+const MainStats = styled.div`
+ flex : 2;
+ width: 100% ;
+ `;
+const Title = styled.div`
+ p{
+    font-size: 14px;
+    color : "rgba(234, 234, 255, 0.68)", 
+ }
+ `;
